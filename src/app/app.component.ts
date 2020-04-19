0ea4077fcf6d6009f8fb11669ba8817dbf39320a
jinglanfan <2154242914@qq.com>
2020-04-19 16:56:31 +0800

    第一次

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MENUS, PRODUCTS } from './data';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  menus = MENUS;
+
+  products = PRODUCTS;
+
+}
